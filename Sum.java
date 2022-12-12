633293e1818212c2b1fb2cbfd21b9bdc10f8a7ff
akgoel476 <akgoel476@gmail.com>
2022-12-12 23:48:12 +0530

    Updated

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the first Number: ");
+		int a=sc.nextInt();
+		System.out.print("Enter the Second Number: ");
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.print(c);
+
+	}
+
+}
